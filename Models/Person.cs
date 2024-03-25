7a0dbc38cb564ac42e9846455186a5787a35ee88
Guilherme do Amaral Gonçalves da Silva <guilhemesilva2227@gmail.com>
2024-03-25 20:38:32 -0300

    first commit

---
+++ Models/Person.cs
@@ -0,0 +1,10 @@
+using System.Reflection.Metadata;
+
+public class Person
+{
+    public string Name {get; set;}
+    public string Gender {get; set;}
+    public int Height {get; set;}
+    public  int Age {get; set;}
+    public bool Alive {get; set;}
+}
